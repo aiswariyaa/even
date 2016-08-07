7897aa1382d1541dbea0e3fc48f06829cd06976f
aiswariyaa <giswarya39@gmail.com>
2016-08-07 15:47:01 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,14 @@
+import java util.Scanner;
+public class Name
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+System.out.println("enter the number of times to print the word :");
+int n=in.nextInt();
+for(int i=0;i<=n;i++)
+{
+System.out.println("hello");
+}
+}
+} 
