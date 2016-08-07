19ad7b3bbfc376185bd6aea1e5d226d304a42900
aiswariyaa <giswarya39@gmail.com>
2016-08-07 17:38:18 +0530

    Add files via upload

---
+++ large.java
@@ -0,0 +1,20 @@
+import.java.io.*;
+public class large
+{
+public static void main(String args[]){
+int a=10;
+int b=20;
+int c=5;
+if(a>b&&a>c)
+{
+System.out.println("the largest number is a");
+}
+else if(b>a&&b>c)
+{
+System.out.println("the largest number is b");
+}
+else 
+{
+System.out.println("the largest number is c");
+}}
+}
