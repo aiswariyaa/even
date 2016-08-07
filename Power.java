f86d81478080f3f5e0d5a712706835f5eda4b9b7
aiswariyaa <giswarya39@gmail.com>
2016-08-07 17:43:39 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Power{
+public static void main(String args[]){
+Scanner in=new Scanner(System.in);
+System.out.println("enter the number to find the power :");
+int a=in.nextInt();
+int p=Math.Pow(a,a);
+}
+}
